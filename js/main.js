2c321b5f53785e708b53e54977cfed50da1a5a47
tylerziegelman <tylerziegelman+git@gmail.com>
2018-10-25 23:54:01 -0600

    added moving background image, prefixer.js, and firefox specific styles

--- js/main.js
+++ js/main.js
@@ -11,6 +11,21 @@ const revertBlur = () => {
     blur.style.filter = "blur(0px)";
     menu.style.filter = "blur(0px)";
 }
+
+function move(e){
+    var image = document.getElementById('move'),
+        w = window.innerWidth,
+        h = window.innerHeight,
+        mouseX = e.clientX,
+        mouseY = e.clientY,
+        imageX = (mouseX/w)*50,
+        imageY = (mouseY/h)*80;
+     
+     image.style["background-position"] = imageX + "% " + imageY + "%";
+   }
+
+
+window.addEventListener('mousemove', move);
 menu.addEventListener('click', createBlur);
 document.addEventListener('click', revertBlur)
 
